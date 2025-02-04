c453c81e90ccf0376048fecfcd85ce69a14ab403
Niles Bontrager <artoftheniles@yahoo.com>
2025-02-03 22:31:47 -0500

    Header Styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,8 +7,15 @@ import '../assets/styles/Header.css'
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="logo-snap">Snap</h1>
-      <h1 className="logo-bites">Bites</h1>
+      <span className="logo-container">
+        {/* Icon */}
+        <h1 className="logo-snap">Snap</h1>
+        <h1 className="logo-bites">Bites</h1>
+      </span>
+      <span className="profile-container">
+        <h2 className="profile-name">John Doe</h2>
+        <button className="login-button">Login</button>
+      </span>
     </header>
   )
 }
