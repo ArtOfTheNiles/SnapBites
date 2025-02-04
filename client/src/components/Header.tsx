e5f36073f42bd6c8f20ff5f78197025eb93e4fc4
Niles Bontrager <artoftheniles@yahoo.com>
2025-02-03 21:02:42 -0500

    Components scaffolded with dedicated style sheets

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,5 @@
+// Branding
+// If not logged in, show the login button
+// If logged in, show the profile name and settings
+// If not present on the index while logged in, show the log meal button
+import '../assets/styles/Header.css'
---
+++ client/src/components/MealCard.tsx
@@ -0,0 +1,3 @@
+// A quick representation of a meal for the user
+// Should have an image, name, and buttons for editing or favoriting
+import '../assets/styles/MealCard.css'
