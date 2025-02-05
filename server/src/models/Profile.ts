e80939b6f7f362aa88d21f740d2c90439deb8450
Nolan Marshke <nmarshke@gmail.com>
2025-02-04 19:41:27 -0500

    added a profile model + added password verification

--- server/src/models/Profile.ts
+++ server/src/models/Profile.ts
@@ -1,3 +1,14 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../config/connection';
 import bcrypt from 'bcrypt';
+
+
+class Profile extends Model {
+    public id!: number;
+    public username!: string;
+    public password!: string;
+
+    public async validatePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
+}
