c19c50ac0ef68cc8eb58f389ae1b8f3b4840a1ec
Nolan Marshke <nmarshke@gmail.com>
2025-02-04 20:01:47 -0500

    completed the profile model

--- server/src/models/Profile.ts
+++ server/src/models/Profile.ts
@@ -12,3 +12,36 @@ class Profile extends Model {
         return bcrypt.compare(password, this.password);
     }
 }
+
+Profile.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},
+{
+    sequelize,
+    modelName: 'profile',
+    tableName: 'profiles',
+    timestamps: false,
+    hooks: { 
+        beforeCreate: async (profile) => {
+            if (profile.changed('password')) {
+                profile.password = await bcrypt.hash(profile.password, 10);
+                }
+            }
+        }
+    }
+);
+
+export default Profile;
