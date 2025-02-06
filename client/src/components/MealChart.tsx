348873d410f43ccd20ba0b2c70b2b09ea012d15e
Niles Bontrager <artoftheniles@yahoo.com>
2025-02-06 01:06:41 -0500

    MaterialUI dependencies added, let's get some sliders🍔

--- client/src/components/MealChart.tsx
+++ client/src/components/MealChart.tsx
@@ -1,5 +1,8 @@
 // Trying to make a sub-component for the meal chart
 // Need to find a way to make the chart responsive
+// Radar chart failed because of not enough data points to make it look good
+// rc-slider is a possiblity, but docs are poor and downloads stopped
+// https://mui.com/material-ui/react-slider/
 
 export default function MealChart() {
   return (
