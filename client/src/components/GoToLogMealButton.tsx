2a9f2a0ec3d63258f10854b42e7348e04fc7f7bb
Niles Bontrager <artoftheniles@yahoo.com>
2025-02-08 23:28:32 -0500

    Log Meal Component linked and icon added

--- client/src/components/GoToLogMealButton.tsx
+++ client/src/components/GoToLogMealButton.tsx
@@ -1,9 +1,16 @@
 // This is the Log Meal button, which is central to the app and is used to log a meal.
 // Should have added flair, but be repeatable throughout the app
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBurger } from '@fortawesome/free-solid-svg-icons'
 import '../assets/styles/LogMealButton.css'
 
 export default function GoToLogMealButton() {
   return (
-    <button className="log-meal-button">+</button>
+  <>
+    <Link to="/logmeal" className="log-meal-link">
+      <FontAwesomeIcon icon={faBurger} />
+    </Link>
+  </>
   )
 }
