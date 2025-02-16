e621d15e0aa7f5e18971bbe0834433333a569293
Niles Bontrager <artoftheniles@yahoo.com>
2025-02-15 23:55:53 -0500

    Vite Environment Interfaces added

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 declare module 'react-dom/client';
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly ADMIN_SECRET: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
